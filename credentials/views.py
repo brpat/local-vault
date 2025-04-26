9e5a22c3959649b894cc777c2282bf763a7650c2
Brijesh Patel <brijeshpatel@Brijeshs-Laptop.local>
2025-04-26 17:07:13 -0400

    Added user registration, signin templates

--- credentials/views.py
+++ credentials/views.py
@@ -5,10 +5,27 @@ from django.http import HttpResponse
 from django.template import loader
 
 
+def base(request):
+    return render(request, 'templates/base.html')  # Simple reference 
+
 def index(request):
     app_title:str = "Local Vault"
     template = loader.get_template('templates/index.html')
     context = {
         'app_title': app_title,
     }
+    return HttpResponse(template.render(context, request))  # dynamic variable rendering
+
+
+def login(request):
+    template = loader.get_template("templates/login.html")
+    context = {}
+    
+    return HttpResponse(template.render(context, request))
+
+
+def signup(request):
+    template = loader.get_template("templates/signup.html")
+    context = {}
+    
     return HttpResponse(template.render(context, request))
