101bc2e75b4ea521f73117fcc47658ba88720b0b
Brijesh Patel <brijeshpatel@Brijeshs-Laptop.local>
2025-04-25 21:11:25 -0400

    Basic models

--- credentials/models.py
+++ credentials/models.py
@@ -4,8 +4,19 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 
-class User(AbstractUser):
+class Vault_User(models.Model):
     username = models.CharField(max_length=14, primary_key=True)
-    password = models.CharField(max_length=30, min_length=10)
+    password = models.CharField(max_length=30)
 
     is_superuser = models.BooleanField()
+
+
+class Credential(models.Model):
+    name = models.CharField(max_length=20, primary_key=True)
+    website = models.CharField(max_length=20)
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    notes = models.TextField(max_length=100)
+
+    def __str__(self):
+        return self.website
