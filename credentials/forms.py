20abdea65f8f0e15fca0786d54ed5abcdd5886c8
Brijesh Patel <brijeshpatel@Brijeshs-Laptop.local>
2025-05-23 18:43:49 -0400

    Switched from CharField to TextField plus aligned text length with models

--- credentials/forms.py
+++ credentials/forms.py
@@ -1,12 +1,39 @@
 from django import forms
+from .models import Vault_User
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 class SignUpForm(forms.Form):
-    username = forms.CharField(max_length=14, label="Username")
-    password = forms.CharField(max_length=30, label="Password", widget=forms.PasswordInput)
-    first_name = forms.CharField(max_length=30, label="First_Name")
-    last_name = forms.CharField(max_length=30, label="Last_Name")
+    username = forms.CharField(max_length=14, required=True)
+    password = forms.CharField(max_length=30, required=True, widget=forms.PasswordInput)
+    user_first_name = forms.CharField(max_length=30, required=True)
+    user_last_name = forms.CharField(max_length=50, required=True)
 
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=14, label="Username")
-    password = forms.CharField(max_length=30, label="Password", widget=forms.PasswordInput)
+    class Meta:
+        model = Vault_User
+        fields = ['username', 'password', 'user_first_name', 'user_last_name']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_repeat = cleaned_data.get('password_repeat')
+
+        if password and password_repeat and password != password_repeat:
+            raise ValidationError("Passwords do not match.")
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = Vault_User(
+            username=self.cleaned_data['username'],
+            user_first_name=self.cleaned_data['user_first_name'],
+            user_last_name=self.cleaned_data['user_last_name'],
+            is_superuser=False,
+        )
+        user.set_password(self.cleaned_data['password'])  # Use set_password()
+        if commit:
+            user.save()
+        return user
+
+    
+
 
