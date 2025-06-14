ed9292f9e001de1e172813db3e1c28822d549c0f
Brijesh Patel <brijeshpatel@Brijeshs-Laptop.local>
2025-06-14 17:17:58 -0400

    Added credential creation form

--- credentials/urls.py
+++ credentials/urls.py
@@ -6,8 +6,10 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("base", views.base, name="base"),
     path("login", views.login, name="login"),
+    path("logout", views.logout, name="logout"),
     path("signup", views.signup, name="signup"),
     path("postsignup", views.postsignup, name="postsignup"),
-    path('myvault', views.myvault, name='myvault')
+    path("add_credential", views.add_credential, name="add_credential"),
+    path('myvault', views.myvault, name='myvault'),
 ]
 
